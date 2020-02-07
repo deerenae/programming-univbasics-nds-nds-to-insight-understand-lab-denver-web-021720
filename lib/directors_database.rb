ccfd8186ace38dc90d5179088887ee92333c9dad
Denise Magner <deerenae@gmail.com>
2020-02-07 22:12:42 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -11,8 +11,9 @@ end
 row_index = 0 
   while row_index < 1 do 
   
-      puts "#{directors_database[row_index][:movies][:title]}"
-    
+   puts "#{directors_database[row_index][:movies][:title]}"
+   binding.pry 
+   
   row_index += 1   
   
   end  
