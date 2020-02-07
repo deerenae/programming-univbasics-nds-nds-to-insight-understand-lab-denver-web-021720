619241525272c24f32996c903baa380491b9cf85
Denise Magner <deerenae@gmail.com>
2020-02-07 22:03:46 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -3,4 +3,26 @@ require 'yaml'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
+  
+  
 end
+
+row_index = 0 
+  while row_index < 1 do 
+    
+    puts " #{directors_database[row_index]}"
+    
+    column_index = 0 
+    while column_index < 1 do
+      puts "#{directors_database[row_index][column_index]}"
+      
+      column_index +=1
+      
+    end 
+    
+  row_index += 1   
+  
+end  
+
+#pp directors_database
+
